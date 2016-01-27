a853285d6be8e29541500c08fcb76072485e2a51
Sezer <sezer.kulac@hotmail.com>
2016-01-27 01:01:15 +0100

    Functionality works

--- oldschool calculator/ViewController.swift
+++ oldschool calculator/ViewController.swift
@@ -7,19 +7,122 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Equals = "="
+        case Empty = "Empty"
+    }
+
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    var operands = [Double]()
+    var currentOperation: Operation = Operation.Equals
+    var userInTheMiddleOfEnteringNumber = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+           print(err.debugDescription)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numberPressed(btn: UIButton!){
+        btnSound.play()
+        
+        if !userInTheMiddleOfEnteringNumber  {
+            // first number can't be 0
+            if btn.tag != 0 {
+                outputLabel.text = "\(btn.tag)"
+                userInTheMiddleOfEnteringNumber = true
+            }
+            
+            // if current operation is equal then clear the operands
+            if currentOperation == Operation.Equals {
+                operands.removeAll()
+            }
+        } else {
+            outputLabel.text = outputLabel.text! + "\(btn.tag)"
+        }
     }
 
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(Operation.Divide)
+    }
 
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(Operation.Multiply)
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(Operation.Subtract)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(Operation.Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(Operation.Equals)
+    }
+    
+    func processOperation(op: Operation) {
+        playSound()
+        
+        // only addpend the display once
+        // this guards against cases where user keeps hitting operations
+        if userInTheMiddleOfEnteringNumber {
+            operands.append(Double(outputLabel.text!)!)
+        }
+        
+        // if at least two operators then do some math
+        if operands.count > 1 {
+            let leftOperand = operands.first!
+            operands.removeFirst()
+            
+            let rightOperand = operands.first!
+            operands.removeFirst()
+            
+            var answer = 0.0
+            if currentOperation == Operation.Multiply {
+                answer = leftOperand * rightOperand
+            } else if currentOperation == Operation.Divide {
+                answer = leftOperand / rightOperand
+            } else if currentOperation == Operation.Add {
+                answer = leftOperand + rightOperand
+            } else if currentOperation == Operation.Subtract {
+                answer = leftOperand - rightOperand
+            }
+            
+            // add the answer back to the front of the array
+            operands.insert(answer, atIndex: 0)
+            outputLabel.text = "\(answer)"
+        }
+        
+        currentOperation = op
+        userInTheMiddleOfEnteringNumber = false
+    }
+    
+    func playSound() {
+        if btnSound.playing {
+            btnSound.stop()
+        }
+        
+        btnSound.play()
+    }
 }
 
